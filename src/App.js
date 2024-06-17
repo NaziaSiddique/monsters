0ffe1329fb01ee72ce29392aecd3cb0a9f814f7f
NaziaSiddique <nazia.siddique04@gmail.com>
2024-06-17 19:20:37 +0100

    Added an event when button clicks to change name by setting state

--- src/App.js
+++ src/App.js
@@ -18,7 +18,11 @@ class App extends Component {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hi {this.state.name}</p>
-        <button>Change Name</button>
+        <button
+        onClick={() => {
+          this.setState({name: 'Nusaybah'});
+        }}
+        >Change Name</button>
       </header>
     </div>
   );
