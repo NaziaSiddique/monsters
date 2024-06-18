03f44bcee7559232afc0bfdef09b8fa9244cd9fc
NaziaSiddique <nazia.siddique04@gmail.com>
2024-06-18 08:38:21 +0100

    used state to shallow merge variable changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
   super();
    
   this.state = {
-    name: 'Nazia;'
+    name: {firstName:'Nazia', lastName: 'Siddique'},
+    company: 'nCino',
   }
  }
 
@@ -17,10 +18,11 @@ class App extends Component {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hi {this.state.name}</p>
+        <p>Hi, {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}.
+        </p>
         <button
         onClick={() => {
-          this.setState({name: 'Nusaybah'});
+          this.setState({name: {firstName: 'Nusaybah', lastName: 'Bint-Siddique'}, company: 'Dev'});
         }}
         >Change Name</button>
       </header>
