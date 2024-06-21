2fbbe055eaf790d177e745ccd0c4ebd84740a202
NaziaSiddique <nazia.siddique04@gmail.com>
2024-06-21 04:46:33 +0100

    Using keys to map each element

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import {Component} from 'react';
 
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -11,15 +11,19 @@ class App extends Component {
     monsters: [
       {
         name: 'Linda',
+        id: '12e1231e',
       },
       {
         name: 'Frank',
+        id: '12ed2d1cs',
       },
       {
         name: 'Jacky',
+        id: '12e31e',
       },
       {
         name: 'Andrei',
+        id: '12e1e213',
       }
     ]
   };
@@ -30,7 +34,11 @@ class App extends Component {
    <div className="App">
     {
       this.state.monsters.map((monster) => {
-        return<h1>{monster.name}</h1>
+        return (
+        <div key={monster.id}>
+        <h1>{monster.name}</h1>
+        </div>
+        );
       })
     }
    </div>
